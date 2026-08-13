1e3feb40eca37127039714c92f416165308715de
Xychen <dmark.soliman1027@gmail.com>
2026-08-13 12:34:10 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dhvukkpqwyzayeosnzsg.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_zi28TZAf5-6-1RXmnsWGrA_tNMLIeFZ";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
